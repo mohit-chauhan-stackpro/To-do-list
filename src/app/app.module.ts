ba46abf448fdbcbd140a6917832e54b2b952d11e
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-20 17:55:04 +0530

    added input and button nd added new comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
